131cec7bd9d5f8021e57993140c388cda2c823fa
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:26:29 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,4 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "vpc_peering" {
+  value       = module.vpc_peering
+  description = "VPC peering outputs"
 }
